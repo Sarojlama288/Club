3ca663ef77ab11515f5f3e4337098d26ba4a1ba1
Sarojlama288 <saronlama8@gmail.com>
2026-03-12 10:27:41 +0545

    Function , loops

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-// console.log("Hello World");
-console.log("My name is Saroj Lama");
+/* let x = 10;
+let y = 20;
+if (x>y){
+    console.log("X is greatest")
+ 
+} else {
+    console.log("Y is greatest")
+} */
+/* let age = 20;
+let country = "Nepal"
+let text = "You can drive"
+if (country == "Nepal && age>=20"){
+text = "You can drive";
+} else{
+    text = "You can drive"
+}
+console.log(text); */
